ce7cb7f2d064fe9419055e2e1b8de0d98535e250
Tanner Samples <83928312+TS-Programming@users.noreply.github.com>
2024-04-02 23:45:53 -0700

    changed favicon

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,14 +2,14 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/tannerSamples.glb?v=1712090060619");
+needle_exported_files.push("assets/tannerSamples.glb?v=1712097715413");
 
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1712090060619");
+		needleEngine.setAttribute("hash", "1712097715413");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
